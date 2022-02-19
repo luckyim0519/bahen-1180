006adfc636cdbcd96abd0895e5755d50e4dab926
Bantonio12 <thcha0727@gmail.com>
2022-02-19 09:24:57 -0500

    added firebase auth

--- src/login/Register.js
+++ src/login/Register.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styles from "./login.module.css";
 import { useNavigate } from "react-router-dom";
 import { pageTransition, pageVariants } from "../Transitions";
-
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { motion } from "framer-motion";
 
 function Register() {
@@ -10,9 +10,12 @@ function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const auth = getAuth();
 
   const handleClick = () => {
     console.log("Registering new user now!!");
+
+    
     // firebase stuff
     let registerSuccess = true;
 
