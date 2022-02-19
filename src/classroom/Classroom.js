60e94e9e3b9695d315ebb42ebce5cb9e8bb8adb2
tttzach <tttzach@gmail.com>
2022-02-19 10:57:37 -0500

    Link books to pdf files and add tooltip

--- src/classroom/Classroom.js
+++ src/classroom/Classroom.js
@@ -5,6 +5,12 @@ import SmallTable from "./SmallTable";
 import BigTable from "./BigTable";
 import styles from "./assets/chalkboard.module.css";
 import bookstyles from "./assets/book.module.css";
+import course_outline from "./assets/co370_course_outline.pdf";
+import homework from "./assets/co370_hw4.pdf";
+import week4 from "./assets/co370_week4.pdf";
+import week6a from "./assets/co370_notes_week6a.pdf";
+import practice_test from "./assets/co370_practice_test1.pdf";
+import test_solution from "./assets/co370_test1_soln.pdf";
 
 import { pageTransition, pageVariants } from "../Transitions";
 
@@ -37,13 +43,43 @@ function Classroom() {
         <BigTable></BigTable>
         <BigTable></BigTable>
       </div>
+
       <div className={bookstyles.bookshelf}>
-        <Book bookUrl="https://student.cs.uwaterloo.ca/~cs451/"></Book>
-        <Book bookUrl="https://student.cs.uwaterloo.ca/~cs451/"></Book>
-        <Book bookUrl="https://student.cs.uwaterloo.ca/~cs451/"></Book>
-        <Book bookUrl="https://student.cs.uwaterloo.ca/~cs451/"></Book>
-        <Book bookUrl="https://student.cs.uwaterloo.ca/~cs451/"></Book>
-        <Book bookUrl="https://student.cs.uwaterloo.ca/~cs451/"></Book>
+        <div className={bookstyles.tooltip}>
+          <span className={bookstyles.tooltiptext}>Course Outline</span>
+          <Book bookUrl={course_outline}>
+          </Book>
+        </div>
+
+        <div className={bookstyles.tooltip}>
+          <span className={bookstyles.tooltiptext}>Homework 4</span>
+          <Book bookUrl={homework}>
+          </Book>
+        </div>
+
+        <div className={bookstyles.tooltip}>
+          <span className={bookstyles.tooltiptext}>Week 4 Notes</span>
+          <Book bookUrl={week4}>
+          </Book>
+        </div>
+
+        <div className={bookstyles.tooltip}>
+          <span className={bookstyles.tooltiptext}>Week 6a Notes</span>
+          <Book bookUrl={week6a}>
+          </Book>
+        </div>
+
+        <div className={bookstyles.tooltip}>
+          <span className={bookstyles.tooltiptext}>Practice Test 1</span>
+          <Book bookUrl={practice_test}>
+          </Book>
+        </div>
+
+        <div className={bookstyles.tooltip}>
+          <span className={bookstyles.tooltiptext}>Test 1 Solution</span>
+          <Book bookUrl={test_solution}>
+          </Book>
+        </div>
       </div>
     </motion.div>
   );
